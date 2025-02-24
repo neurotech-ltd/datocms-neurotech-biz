b9ae94ce7a738756d2dfbf741209b6d74b288322
Emile Swain <Emile.Swain@akqa.com>
2025-02-24 15:12:44 +0000

    styled project cards

--- src/lib/components/ContentBlocks/ProjectCardsBlock/index.svelte
+++ src/lib/components/ContentBlocks/ProjectCardsBlock/index.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <div class="project-cards-block">
-    <div class="project-cards-block-header">
+    <div class="project-cards-header">
         <h1>{unmaskedBlock.title}</h1>
         {#if unmaskedBlock.description }
             <p>{unmaskedBlock.description}</p>
@@ -29,8 +29,8 @@
                 </div>
                 <div class="project-card-details">
                     <h1>{project.title}</h1>
-                    <p>{JSON.stringify(project.description, null, 4)}</p>
-                    <a href="/project/{project.slug}" rel="no-prefetch" aria-label="{project.slug}"> More</a>
+                    <p>{JSON.stringify(project.tagLine, null, 4)}</p>
+                    <a href="/project/{project.slug}" rel="no-prefetch" aria-label={project.slug}> More</a>
                 </div>
             </div>
         {/each}
@@ -39,7 +39,7 @@
 
 <style>
     /* set default margins of content.*/
-    .projects-cards-block {
+    .project-cards-block {
         display: flex;
         flex-direction: column;
     }
@@ -52,38 +52,50 @@
         padding: var(--page-margin);
     }
 
-    .project-card-header h1 {
+    .project-cards-header h1 {
         margin-bottom: 0;
     }
 
-    .project-list {
+    .project-cards-list {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin-left: 32px;
+        margin-right: 32px;
     }
 
     .project-card {
         display: flex;
-        flex-direction: column-reverse;
+        flex-direction: column;
         margin-bottom: 16px;
         min-height: 520px;
+        max-width: 300px;
     }
 
     @media (min-width: 1024px) {
         .project-card {
-            flex-direction: row;
+            flex-direction: column;
         }
     }
 
     .project-card-image {
-        flex: 1;
-        height: 100%;
+        width: 300px;
+        height: 300px;
+        display: flex; /* Center the image within the div */
+        justify-content: center;
+        overflow: hidden;
+    }
+
+    :global(.project-card-image > picture){
+        display: flex; /* This is really important otherwise it doesn't work. */
     }
 
     :global(.project-card-image .project-card-image-control) {
-        /*need to sort out image sizing.*/
-        height: 550px !important;
-        max-height: 560px !important;
-        object-fit: cover !important;
+        width: 100%;
+        height: 100%;
+        object-fit: cover; /* Ensures the image covers the entire area */
+        display: block;
     }
 
     .project-card-details {
@@ -106,14 +118,4 @@
         align-self: center;
     }
 
-    .on_light {
-        /*    Dark text on light background */
-        background-color: #eaeaea;
-        --text-color: #1C346C;
-    }
-
-    .on_dark {
-        background-color: #1C346C;
-        --text-color: white;
-    }
 </style>
