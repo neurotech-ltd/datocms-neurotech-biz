e2fc2b345b80e6c84f45144c70b9ffc3d6620a5a
Emile Swain <Emile.Swain@akqa.com>
2025-02-28 16:23:38 +0000

    updated to svelte5 syntax

--- src/routes/project/[slug]/+page.svelte
+++ src/routes/project/[slug]/+page.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
   import {Head, querySubscription} from '@datocms/svelte';
-  import type {ProjectData} from './$types';
   import PageBloc from "../../../lib/components/PageBloc/index.svelte";
   import LayoutBloc from "../../../lib/components/LayoutBloc/index.svelte";
   import NavigationBlock from "../../../lib/components/NavigationBlock/index.svelte";
   import ContentBlocks from "../../../lib/components/ContentBlocks/index.svelte";
 
-  // just in case it becomes a problem https://github.com/lazd/iNoBounce
-
-  export let data: ProjectData;
-  $: subscription = querySubscription(data.subscription);
-  $: project = $subscription.data?.project;
+  let {data, isBrowser}: Props = $props();
+  let subscription = $derived(querySubscription(data.subscription));
+  let project = $derived($subscription.data?.project);
 
 </script>
 
